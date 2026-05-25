35956aed8c2c0ff0a1d10d34f04a75ff07721a61
Neha020305 <nehanreddy2305@gmail.com>
2026-05-26 00:17:50 +0530

    Prepare project for Railway deployment

--- anugya-hackathon-main/backend/server.js
+++ anugya-hackathon-main/backend/server.js
@@ -54,7 +54,7 @@ app.use((err, req, res, next) => {
 
 // START SERVER
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' , () => {
   console.log(`Server running on http://localhost:${PORT}`);
   console.log('Environment:', process.env.NODE_ENV || 'development');
 });
